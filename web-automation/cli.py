f5391f0ad6f9fac6fdb4b308da35efcfdde490ff
Sebastian Witowski <witowski.sebastian@gmail.com>
2025-06-12 21:31:57 +0200

    updated cli file for web-automation

--- web-automation/cli.py
+++ web-automation/cli.py
@@ -2,7 +2,7 @@ import asyncio
 
 import click
 
-from .api import add_access
+from .api import add_access, remove_access
 
 
 @click.group()
@@ -10,7 +10,7 @@ def cli():
     pass
 
 
-@click.command()
+@cli.command()
 @click.argument("user")
 @click.argument("script")
 @click.argument("until", required=False)
@@ -32,6 +32,22 @@ def add(user, script, until):
             expiration = until
 
     result = asyncio.run(add_access(script_fullname, user, trial, expiration))
+    click.echo(result)
+
+
+@cli.command()
+@click.argument("user")
+@click.argument("script")
+def remove(script, user):
+    script_fullname = None
+    if script == "1":
+        script_fullname = "Script1"
+    elif script == "2":
+        script_fullname = "Script2"
+    else:
+        raise ValueError(f"Wrong script name {script}! Choose '1' or '2'")
+
+    result = asyncio.run(remove_access(script_fullname, user))
     click.echo(result)
 
 
